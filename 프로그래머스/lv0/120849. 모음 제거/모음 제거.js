8e7d04d5c9fc6933642e8c67c2ae79aa540c306c
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2023-09-07 23:56:08 +0900

    [level 0] Title: 모음 제거, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,7 +1,4 @@
 function solution(my_string) {
-    const removeList = ['a','e','i','o','u'];
-    
-    return my_string.split('').map((str) => {
-        return removeList.indexOf(str) > -1 ? '' : str
-    }).join('');
+    var answer = '';
+    return answer;
 }
