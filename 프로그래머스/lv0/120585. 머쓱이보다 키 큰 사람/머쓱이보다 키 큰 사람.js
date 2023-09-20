19555f7a6c0f6e00b9a6c1aab275b5add584d789
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2023-09-20 13:37:47 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,7 +1,6 @@
 function solution(array, height) {
     var answer = 0;
     for(let i=0; i<array.length; i++){
-        console.log(i,array[i])
         if(array[i]>height) ++answer;
     }
     return answer;
