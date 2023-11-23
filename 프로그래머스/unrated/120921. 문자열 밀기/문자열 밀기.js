c096f47a75eb3b386fe83facff5ad11ef97dbc30
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2023-11-23 16:02:22 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.js
@@ -1,16 +1,4 @@
 function solution(A, B) {
     var answer = 0;
-    
-    while(A != B && answer<A.length){
-        answer ++;
-        
-        A = A.split('');
-        
-        const popStr = A.pop();
-        A = [popStr, ...A]
-        
-        A = A.join('');
-    }
-    
-    return A != B ? -1 : answer;
+    return answer;
 }
