0d3c870de72e9fdcf1399b949ce4ee9239f160ac
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2023-11-24 17:00:20 +0900

    [level unrated] Title: 다음에 올 숫자, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/unrated/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,14 +1,13 @@
 function solution(common) {
     var answer = 0;
-    //등비인지 등차인지 체크 등차 0 등비는 1 ;
-    const check = common[1] - common[0] === common[2] - common[1] ? 0 : 1
-
-    if(check === 0){
+    
+    //등비인지 등차인지 체크;
+    if(common[1] - common[0] === common[2] - common[1]){
         //등차
-        answer = common[common.length-1] + common[1] - common[0];
+        answer = common.pop() + common[1] - common[0];
     }else{
         //등비
-        answer = common[common.length-1] * (common[1]/common[0]);
+        answer = common.pop() * (common[1]/common[0]);
     }
     
     return answer;
