4b93f640bcca2ba92f7f1d220b37e9294ac8aba9
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2023-11-25 10:52:36 +0900

    [level 2] Title: 의상, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,13 +1,13 @@
 function solution(clothes) {
     const user = {}
     let answer = 1;
+    
     clothes.forEach((v) => {
         if(user[v[1]] === undefined) user[v[1]] = 1;
         user[v[1]]++;
     })
     
-    Object.keys(user).forEach((v) => {
-        answer *= user[v];
-    })
-    return answer - 1;
+    return Object.keys(user).reduce((acc, key) => {
+        return acc*user[key];
+    },1) - 1;
 }
