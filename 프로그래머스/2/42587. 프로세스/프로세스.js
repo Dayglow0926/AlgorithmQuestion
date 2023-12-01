31d54b6ad9b5972f5ff225534000926c93a2fb98
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2023-12-01 12:34:51 +0900

    [level 2] Title: 프로세스, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,22 @@
+function solution(priorities, location) {
+    var answer = 1;
+    const prioritiesTemp = [...priorities];
+    priorities.sort((a,b) => b-a);
+    
+    while(true){
+        if ( prioritiesTemp[0] === priorities[0] ) {
+            if(location === 0) return answer;
+            prioritiesTemp.shift();
+            priorities.shift();
+            answer++;
+            location--;
+        }else{
+            prioritiesTemp.push(prioritiesTemp.shift());
+            if(location === 0) location = prioritiesTemp.length-1;
+            else location--;
+        }
+    } 
+    
+    
+    return answer;
+}
