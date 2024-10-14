1909b409e1fb26d0e8a614ae924814a8d0fa23c3
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2024-10-14 16:24:41 +0900

    [level 3] Title: 금과 은 운반하기, Time: 395.90 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.js
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.js
@@ -1,39 +1,45 @@
-function search(a, b, g, s, w, t, mid) {
+function apply(a, b, g, s, w, t, mid){
     let totalGold = 0;
-    let totalSilver = 0;
-    let totalMineral = 0;
-
-    for (let i = 0; i < g.length; i++) {
+    let totalSilver = 0;    
+    let totalMineral = 0;    
+    
+    for(let i = 0; i < g.length; i++){
         const time = t[i];
+        
         const roundTime = time * 2;
-        let moveCnt = Math.floor(mid / roundTime);
-        if (mid % roundTime >= time) moveCnt++;
-
-        const maxTake = w[i] * moveCnt;
-
+        let moveCount = parseInt(mid / roundTime); 
+        if(mid % roundTime >= time) moveCount++;
+        const maxTake = w[i] * moveCount;
+        
         totalGold += Math.min(g[i], maxTake);
         totalSilver += Math.min(s[i], maxTake);
         totalMineral += Math.min(g[i] + s[i], maxTake);
     }
-
-    return totalGold >= a && totalSilver >= b && totalMineral >= (a + b);
+    
+    if(totalGold >=a && totalSilver>=b && totalMineral>=(a+b)) {
+        return true;
+    }
+    
+    return false
+    
 }
 
 function solution(a, b, g, s, w, t) {
-    let start = 0;
-    let end = 10e14 * 3;
-    let answer = end;
-
-    while (start <= end) {
-        const mid = Math.floor((start + end) / 2);
-
-        if (search(a, b, g, s, w, t, mid)) {
+    let startTime = 1;
+    let endTime = 10e14*3;
+    let answer = endTime;
+    
+    while(startTime <= endTime){
+        
+        const mid = parseInt((startTime + endTime) / 2);
+        
+        if(apply(a, b, g, s, w, t, mid)){
             answer = Math.min(answer, mid);
-            end = mid - 1;
-        } else {
-            start = mid + 1;
+            endTime = mid - 1;
+        }else {
+            startTime = mid + 1
         }
     }
-
+    
     return answer;
 }
